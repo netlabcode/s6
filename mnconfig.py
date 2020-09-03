e352cb736e7e694608cc94bbf51f6878b76c9760
APresekal <presekal@ui.ac.id>
2020-09-03 05:10:38 -0400

    Add files via upload

--- mnconfig.py
+++ mnconfig.py
@@ -129,6 +129,7 @@ def run():
     info(os.system('ip addr add 10.1.0.99/24 dev s1'))
     info(os.system('ip link set s1 up'))
     
+    """
     info( net[ 'm1' ].cmd( 'python3 s06m01.py &amp' ) )
     info( net[ 'm2' ].cmd( 'python3 s06m02.py &amp' ) )
     info( net[ 'm3' ].cmd( 'python3 s06m03.py &amp' ) )
@@ -136,6 +137,7 @@ def run():
     info( net[ 'm5' ].cmd( 'python3 s06m05.py &amp' ) )
     info( net[ 'm6' ].cmd( 'python3 s06m06.py &amp' ) )
     #info( net[ 'm3' ].cmd( 'python3 hello.py' ) )
+    """
     
     CLI( net )
     net.stop()
