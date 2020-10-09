70a790fbee9793d5a0dc03d306dd24c904156fd5
APresekal <presekal@ui.ac.id>
2020-10-09 12:33:08 -0400

    Add files via upload

--- testsql.py
+++ testsql.py
@@ -2,8 +2,20 @@
 import psycopg2
 
 try:
-	conn = psycopg2.connect("host='131.180.165.5:5432' dbname='postgres' user='postgres' password=''")
+	conn = psycopg2.connect("host='131.180.165.5' dbname='postgres' user='postgres' password='c'")
 	print("Connection established")
+
+	cursor = conn.cursor()
+
+	cursor.execute("SELECT name FROM test")
+
+	rows = cursor.fetchall()
+
+	for row in rows:
+		print(row)
+
+	cursor.close()
+
 except:
 	print("Connection Fail")
 
