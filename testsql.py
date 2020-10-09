afb4261cdc649ed1d3bf3f9f040c4a6cd1c90d33
APresekal <presekal@ui.ac.id>
2020-10-09 13:29:17 +0200

    Update testsql.py

--- testsql.py
+++ testsql.py
@@ -2,7 +2,7 @@
 import psycopg2
 
 try:
-	conn = psycopg2.connect("host='131.180.165.5' dbname='postgres' user='postgres' password='crpg'")
+	conn = psycopg2.connect("host='131.180.165.5' dbname='postgres' user='postgres' password='----'")
 	print("Connection established")
 except:
 	print("Connection Fail")
