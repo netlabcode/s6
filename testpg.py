b7f4dcaf2d8e178d0d391716c7e5dc7932978b14
APresekal <presekal@ui.ac.id>
2021-03-14 14:08:29 +0100

    Add files via upload

---
+++ testpg.py
@@ -0,0 +1,39 @@
+import psycopg2
+from datetime import datetime
+ 
+conn = psycopg2.connect(host="131.180.165.7", database="CRoF", user="postgres", password="crpg")
+
+#Check Connection
+
+if conn is not None:
+    print('Connection established to PostgreSQL.')
+else:
+    print('Connection not established to PostgreSQL.')
+
+"""
+#Setting auto commit 
+conn.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+dt = datetime.now()
+
+inserted_values = (
+        dt,
+        123456.123456789123456,
+        123
+    )
+"""
+
+# INSTER VALUE TO TABLE ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#cursor.execute(" INSERT INTO test(dtime, dec, val) VALUES (%s,%s,%s)", inserted_values)
+
+"""
+# Preparing SQL queries to INSERT a record into the database.
+cursor.execute('''INSERT INTO test(val, dec, dtime) VALUES (33, -999.123456, dt)''')
+"""
+
+
+#cursor.close()
+conn.close()
