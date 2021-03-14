d30866b94062ef7c40dd4fe16a6046b41ecaeb63
APresekal <presekal@ui.ac.id>
2021-03-14 15:54:46 +0100

    Add files via upload

--- testpg.py
+++ testpg.py
@@ -11,7 +11,7 @@ else:
 
 
 cursor = conn.cursor()
-cursor.execute('''SELECT Objects from objects WHERE ID=1''')
+cursor.execute('''SELECT params from objects WHERE id=1''')
 result = cursor.fetchone();
 print(result)
 
