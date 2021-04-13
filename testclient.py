cb7cefcabb567479cf6cda7834ffd31e44428889
APresekal <presekal@ui.ac.id>
2021-04-13 18:13:43 +0200

    Add files via upload

--- testclient.py
+++ testclient.py
@@ -15,12 +15,12 @@ PORT4 = 994
 
 
 #OPC ACCESS
-url = "opc.tcp://srv786.tudelft.net:53530/OPCUA/SimulationServer"
+url = "opc.tcp://srv785.tudelft.net:53530/OPCUA/SimulationServer"
 client = Client(url)
 client.connect()
 print("connected to OPC UA Server")
-val1 = client.get_node("ns=7;i=1008")
-val2 = client.get_node("ns=7;i=1009")
+val1 = client.get_node("ns=3;i=1008")
+val2 = client.get_node("ns=3;i=1009")
 
 
 
