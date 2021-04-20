a4a6795a08fd8890948bf97b353e9c071f4e7524
APresekal <presekal@ui.ac.id>
2021-04-20 17:50:09 +0200

    Add files via upload

--- r01x2.py
+++ r01x2.py
@@ -43,56 +43,56 @@ def serverX():
             conn1, addr = s1.accept()
             with conn1:
                 print('Server 1 from:',addr)
+
+                conn = psycopg2.connect(host="131.180.165.7",database="CRoF",user="postgres", password="crpg")
+                  
+                cursor = conn.cursor()
+                
+                #Value id 1-7
+                cursor.execute('''SELECT value from objects WHERE id=1''')
+                result = cursor.fetchone()
+                record1 = result[0]
+                cursor.execute('''SELECT value from objects WHERE id=2''')
+                result = cursor.fetchone()
+                record2 = result[0]
+                cursor.execute('''SELECT value from objects WHERE id=3''')
+                result = cursor.fetchone()
+                record3 = result[0]
+                cursor.execute('''SELECT value from objects WHERE id=4''')
+                result = cursor.fetchone()
+                record4 = result[0]
+                cursor.execute('''SELECT value from objects WHERE id=5''')
+                result = cursor.fetchone()
+                record5 = result[0]
+                cursor.execute('''SELECT value from objects WHERE id=6''')
+                result = cursor.fetchone()
+                record6 = result[0]
+
+                #Value code
+                cursor.execute('''SELECT code from objects WHERE id=1''')
+                result = cursor.fetchone()
+                r1 = result[0]
+                cursor.execute('''SELECT code from objects WHERE id=2''')
+                result = cursor.fetchone()
+                r2 = result[0]
+                cursor.execute('''SELECT code from objects WHERE id=3''')
+                result = cursor.fetchone()
+                r3 = result[0]
+                cursor.execute('''SELECT code from objects WHERE id=4''')
+                result = cursor.fetchone()
+                r4 = result[0]
+                cursor.execute('''SELECT code from objects WHERE id=5''')
+                result = cursor.fetchone()
+                r5 = result[0]
+                cursor.execute('''SELECT code from objects WHERE id=6''')
+                result = cursor.fetchone()
+                r6 = result[0]
+
                 while True:
                     data = "a"
                     datax = data.encode()
                     try:
                         
-                        conn = psycopg2.connect(host="131.180.165.7",database="CRoF",user="postgres", password="crpg")
-
-                        
-                        cursor = conn.cursor()
-                        
-
-                        #Value id 1-7
-                        cursor.execute('''SELECT value from objects WHERE id=1''')
-                        result = cursor.fetchone()
-                        record1 = result[0]
-                        cursor.execute('''SELECT value from objects WHERE id=2''')
-                        result = cursor.fetchone()
-                        record2 = result[0]
-                        cursor.execute('''SELECT value from objects WHERE id=3''')
-                        result = cursor.fetchone()
-                        record3 = result[0]
-                        cursor.execute('''SELECT value from objects WHERE id=4''')
-                        result = cursor.fetchone()
-                        record4 = result[0]
-                        cursor.execute('''SELECT value from objects WHERE id=5''')
-                        result = cursor.fetchone()
-                        record5 = result[0]
-                        cursor.execute('''SELECT value from objects WHERE id=6''')
-                        result = cursor.fetchone()
-                        record6 = result[0]
-
-                        #Value code
-                        cursor.execute('''SELECT code from objects WHERE id=1''')
-                        result = cursor.fetchone()
-                        r1 = result[0]
-                        cursor.execute('''SELECT code from objects WHERE id=2''')
-                        result = cursor.fetchone()
-                        r2 = result[0]
-                        cursor.execute('''SELECT code from objects WHERE id=3''')
-                        result = cursor.fetchone()
-                        r3 = result[0]
-                        cursor.execute('''SELECT code from objects WHERE id=4''')
-                        result = cursor.fetchone()
-                        r4 = result[0]
-                        cursor.execute('''SELECT code from objects WHERE id=5''')
-                        result = cursor.fetchone()
-                        r5 = result[0]
-                        cursor.execute('''SELECT code from objects WHERE id=6''')
-                        result = cursor.fetchone()
-                        r6 = result[0]
 
 
                         """
@@ -114,11 +114,13 @@ def serverX():
                         
                     except:
                         conn1.close()
+                        conn.close()
                         print("Connection Close")
                         break
                 
                 conn1.close()
                 print("Restart Server")
+                conn.close()
                 s1.close()
                 time.sleep(1)
                 serverX()
