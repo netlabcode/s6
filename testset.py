92dba36d2b6822606306381483c93feb1c1f70a1
APresekal <presekal@ui.ac.id>
2021-08-03 11:08:47 +0200

    Add files via upload

--- testset.py
+++ testset.py
@@ -6,7 +6,7 @@ import binascii
 import _thread
 import time
 
-
+###
 HOST = ''
 PORT1 = 991
 PORT2 = 992
@@ -19,10 +19,15 @@ url = "opc.tcp://srv785.tudelft.net:53530/OPCUA/SimulationServer"
 client = Client(url)
 client.connect()
 print("connected to OPC UA Server")
-val1 = client.get_node("ns=7;i=1")
-val2 = client.get_node("ns=7;i=2")
+val1 = client.get_node("ns=7;i=107")
+val2 = client.get_node("ns=7;i=29")
+val3 = client.get_node("ns=7;i=2")
 
 
-value=0
-val1.set_value(value, ua.VariantType.Int16)
+value=107
+value2= 29
+value3= 2
+val1.set_value(value, ua.VariantType.Float)
+val2.set_value(value2, ua.VariantType.Int16)
+val2.set_value(value3, ua.VariantType.Int16)
 
